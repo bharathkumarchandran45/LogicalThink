ba462e4a00a2c957e725e74e6415a93ca84871cf
bharathkumarchandran45 <bharathkumarchandran45@gmail.com>
2026-04-28 09:05:01 +0530

    Added

---
+++ Pattern.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Pattern{
+    public static void main(String[] args) {
+        
+        Scanner ab = new Scanner (System.in);
+
+        System.out.print("Enter The Number Of ROWS : ");
+        int row = ab.nextInt();
+
+        for (int i = 1 ; i <= row ; i++ ){
+            for (int j = 1 ; j <= i ; j++ ){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+    }
+}
+
